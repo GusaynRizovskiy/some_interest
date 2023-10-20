92dcd0865a0d0f9c0502116c461a47afd771ae98
GusaynRizovskiy <makkks200426013@mail.ru>
2023-10-20 20:13:28 +0300

    Add something interesting for me files, will work later

---
+++ generator_function.py
@@ -0,0 +1,8 @@
+a = [23,54,64,23,45,2,1,4,2,3,5,3,6]
+print([i for i in a if i%2==0])
+
+b = [1,2,3,4,5,6,7,8]
+sum = 100
+print([sum+i for i in b])
+
+print([i for i in range(1,10)])
---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+add = lambda x,y: x+y
+umn = lambda x,y: x*y
+dell = lambda x,y: x/y
+min = lambda x,y: x-y
+perim = lambda a,b,c:a+b+c
+plos_squar = lambda a:a**2
+plos_rect = lambda a,b: a*b
+print(add(3,4))
+print(min(100,45))
+print(plos_rect(4,5))
+print(plos_squar(6))
