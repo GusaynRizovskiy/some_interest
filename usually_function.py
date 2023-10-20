d91116328e530e567a7ec5883e2ad009d29991eb
GusaynRizovskiy <makkks200426013@mail.ru>
2023-10-20 20:32:10 +0300

    Добавил простой цикл

---
+++ usually_function.py
@@ -0,0 +1,9 @@
+def perimetr(a,b,c):
+    if a!=0 and b!=0 and c!=0:
+        p = a + b + c
+        return p
+
+def plochad_square(a):
+    return a**2
+print(perimetr(3,4,5))
+print(plochad_square(5))
