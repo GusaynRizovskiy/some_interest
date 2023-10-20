964a1fa7546541660a755682e8a849b4a64ef29f
GusaynRizovskiy <makkks200426013@mail.ru>
2023-10-20 20:55:03 +0300

    Intend need features

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,12 @@
 add = lambda x,y: x+y
 umn = lambda x,y: x*y
-dell = lambda x,y: x/y
 min = lambda x,y: x-y
-perim = lambda a,b,c:a+b+c
 plos_squar = lambda a:a**2
 plos_rect = lambda a,b: a*b
 print(add(3,4))
 print(min(100,45))
 print(plos_rect(4,5))
 print(plos_squar(6))
+print("I love this world, don't see on my problems")
+print("I will be that I want to be for my family")
+print("You think i normally, you are wrong")
--- prostoy_chikl.py
+++ prostoy_chikl.py
@@ -1,4 +1,11 @@
 for i in range(1,200):
     if i%20 == 0:
         print(i)
+print("\n\n")
+A = []
+for j in range(1,10):
+    if j%3==0:
+        A.append(j)
 
+for b in A:
+    print(b)
